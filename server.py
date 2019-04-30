d9c288fa0ab13d82cb86d498c32358d17767a468
ireneacevedo <irene.acevedo.2000@gmail.com>
2019-04-30 11:33:45 +0200

    unfinished server

--- server.py
+++ server.py
@@ -8,11 +8,14 @@ from Seq import Seq
 
 PORT = 8000
 
-ensemblport = 80
 
 SERVER = 'https://rest.ensembl.org'
-headers = ({ "Content-Type" : "text/json"})
-ENDPOINT = ["/info/species?","/overlap/region/human/{}:{}-{}?feature=gene;feature=transcript;feature=cds;feature=exon", '/info/assembly']
+headers = ({ "Content-Type" : "application/json"})
+ENDPOINT = ['/info/species?' , '/info/assembly']
+
+ensemblport = 80
+
+TEST = False
 
 socketserver.TCPServer.allow_reuse_address = True
 
@@ -25,6 +28,10 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         print("  Cmd: " + self.command)
         print("  Path: " + self.path)
 
+        write_test('-----New request----------')
+        write_test("Request line:" + self.requestline)
+        write_test("  Cmd: " + self.command)
+        write_test("  Path: " + self.path)
 
         contents = ""
         try:
@@ -37,32 +44,40 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 self.send_header('Content-Type', 'text/html')
             else:
                 end = self.path.split('?')[0]
+                write_test('Endpoint'+ end)
                 print("End =>", end)
-                    if end == '/listSpecies':
-                            contents = self.attend_info_species()
-                            self.send_response(200)
-                            self.send_header('Content-Type', 'text/html')
-                    elif end == '/karyotype':
-                        contents = self.handle_info_assembly()
-                        self.send_response(200)
-                        self.send_header('Content-Type', 'text/html')
+                if end == '/listSpecies':
+                    contents = self.attend_info_species()
+                    self.send_response(200)
+                    self.send_header('Content-Type', 'text/html')
 
 
-                    elif self.path == '/overlap/region/human/{}:{}-{}?feature=gene;feature=transcript;feature=cds;feature=exon':
-                        contents = self.handle_overlap_region()
-                        self.send_response(200)
-                        self.send_header('Content-Type', 'text/plain')
-                    else:
-                        with open('error.html', 'r') as f:
-                            for i in f:
-                                contents += i
-                                contents = str(contents)
-                        self.send_response(404)
-                        self.send_header('Content-Type', 'text/html')
+                elif end == '/karyotype':
+                    write_test('Endpoint' + end)
+                    contents = self.handle_info_assembly()
+                    self.send_response(200)
+                    self.send_header('Content-Type', 'text/html')
+
+                elif end == '/chromosomeLength':
+                    write_test('Endpoint' + end)
+                    contents = self.handle_chromosome_length()
+                    self.send_response(200)
+                    self.send_header('Content-Type', 'text/html')
+                else:
+                    with open('error.html', 'r') as f:
+                        for i in f:
+                            contents += i
+                            contents = str(contents)
+                    self.send_response(404)
+                    self.send_header('Content-Type', 'text/html')
         except Exception:
             self.send_response(404)
-            with open('hugeerror.html', 'r') as f:
-                contents = f.read()
+            with open('error.html', 'r') as f:
+                for i in f:
+                    contents += i
+                    contents = str(contents)
+            self.send_response(404)
+            self.send_header('Content-Type', 'text/html')
 
 
         self.send_header("Content-Length", len(str.encode(str(contents))))
@@ -75,18 +90,23 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
 
     def attend_info_species(self):
-        r = requests.get(SERVER + ENDPOINT[0], headers = headers)
+        request = SERVER + ENDPOINT[0]
+        r = requests.get(request, headers = headers)
         print("Sending request:", request)
+
+        write_test('Request' + request)
+
         data = r.json()
         print("CONTENT: ")
         print(data)
 
+        write_test('Response' + str(data))
 
         limit = len(data['species'])
 
-        print(data)
 
-        contents = '<!DOCTYPE html><html lang="en"<head><meta charset="UTF-8"><title>Main</title></head><body><h1>Main page</h1>'
+        contents = '<!DOCTYPE html><html lang="en"<head><meta charset="UTF-8"><title>Main</title></head>'\
+                   '<body><h1>Main page</h1>'
         for index in range(limit):
             contents += '<li>'
             contents += data['species'][index]['common name']
@@ -95,26 +115,32 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
 
         return contents
-    def handle_info_assembly(self):
 
-        r = requests.get(SERVER + ENDPOINT[2], headers=headers)
-        data = r.json()
-#http://rest.ensembl.org/info/assembly/homo_sapiens?
+    def handle_info_assembly(self):
         specie = self.path.split("=")[1]
         specie = specie.replace("+", "_")
-        #print("Specie=", specie)
-        request = SERVER + ENDPOINT[2] + "/" + specie
-        print ("Sending request:", request)
 
-        r = requests.get(request, headers=headers)
+        request = SERVER + ENDPOINT[1] + "/" + specie
+        print("Sending request:", request)
+
+        write_test('Request' + request)
+
+        r = requests.get(request, headers = headers)
         data = r.json()
         print("CONTENT: ")
         print(data)
 
+        write_test('Parameters')
+        write_test('Specie:' + str(specie))
+        write_test('Response:' + str(data))
+
+        #http://rest.ensembl.org/info/assembly/homo_sapiens?
+
+
         contents = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Karyotype of ' + specie + '</title></head>' \
                    '<body><h1>Karyotype of ' + specie + '</h1><ol>'
 
-        for index, elem in enumerate(d['karyotype']):
+        for index, elem in enumerate(data['karyotype']):
             contents += "<li>"
             contents += elem #d['karyotype'][index]['common_name']
             contents += "</li>"
@@ -123,33 +149,74 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
         return contents
 
-        return data
+
+    def handle_chromosome_length(self):
+
+        #http://localhost:8000/chromosomeLength?specie=cat&chromo=11
+
+        a = self.path.split('?')[1]
+        b = a.split('&')
+        specie = b[0].split('=')[1]
+        chromo = b[1].split('=')[1]
+
+        specie = specie.replace("+", "_")
+
+        print('specie = ', specie)
+        print('chromo =', chromo)
+
+        request = SERVER + ENDPOINT[1] + "/" + specie
+        print("Sending request:", request)
+
+        write_test('Request' + request)
+
+        r = requests.get(request, headers=headers)
+        data = r.json()
+        print("CONTENT: ")
+        print(data)
+
+        write_test('Parameters')
+        write_test('Specie:' + str(specie))
+        write_test('Chromo' + str(chromo))
+        write_test('Response' + str(data))
 
 
-def handle_overlap_region(self):
-    print("\nConnecting to server: {}:{}\n".format(SERVER, EPORT))
+        lenchromo = ''
+        for i in data['top_level_region']:
+            if i['coord_system'] == 'chromosome' and i['name'] == chromo:
+                if i['name'] == chromo:
+                    lenchromo = i['length']
+        if lenchromo == '':
+            contents = '<!DOCTYPE html><html lang="en" dir="ltr"><head>' \
+                       '<meta charset="UTF-8">' \
+                       '<title>ERROR</title>' \
+                       '</head>' \
+                       '<body style="background-color: red">' \
+                       '<h1>ERROR el nombre "' + chromo + '" no es válido.</h1>' \
+                        '<p>Here there are the websites available: </p>' \
+                        '<a href="/">[main server]</a></body></html>'
+        else:
+            contents = '<!DOCTYPE html> \
+                                            <html lang="en"> \
+                                            <head> \
+                                                <meta charset="UTF-8"> \
+                                                <title>LENGTH OF THE SELECTED CHROMOSOME</title> \
+                                            </head> \
+                                            <body style="background-color: lightblue;"> \
+                                            <body> \
+                                               The length of the chromosome is ' + str(lenchromo) + '. \
+                                            </body> \
+                                            </html>'
+        return contents
 
-    # Connect with the server
-    con = http.client.HTTPConnection(SERVER, EPORT)
-
-    # Send the request message
-    con.request("GET", "/overlap/region/human/{}:{}-{}?feature=gene;feature=transcript;feature=cds;feature=exon")
-
-    # Read the response message from server
-    r1 = con.getresponse()
-
-    # Print the status line
-    print("Response received: {} {}\n".format(r1.status, r1.reason))
-
-    # Read the response's body
-    data = r1.read().decode("utf-8")
-    print("CONTENT: ")
-    print(data)
-    return data
 
 
 
-
+#test
+def write_test(cadena):
+    if TEST == True:
+        f = open('test_report.txt','a')
+        f.write(cadena + '\n')
+        f.close()
 
 Handler = TestHandler
 
@@ -160,4 +227,10 @@ with socketserver.TCPServer(("", PORT), Handler) as httpd:
     except KeyboardInterrupt:
         print('Stop')
 httpd.server_close()
+
+
+
+
+
+
 
