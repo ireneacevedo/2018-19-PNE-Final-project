16bf3d5d5d222480958c4e513565823523f697f0
ireneacevedo <irene.acevedo.2000@gmail.com>
2019-05-05 16:08:27 +0200

    Final server

--- server.py
+++ server.py
@@ -1,21 +1,19 @@
 
 import http.server
 import socketserver
-import json
 import requests
 import http.client
 
 
 PORT = 8000
 
-
 SERVER = 'https://rest.ensembl.org'
+
 headers = ({ "Content-Type" : "application/json"})
+
 ENDPOINT = ['/info/species?' , '/info/assembly']
 
 ensemblport = 80
-
-TEST = False
 
 socketserver.TCPServer.allow_reuse_address = True
 
@@ -23,15 +21,9 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
         print("GET received")
-
         print("Request line:" + self.requestline)
         print("  Cmd: " + self.command)
         print("  Path: " + self.path)
-
-        write_test('-----New request----------')
-        write_test("Request line:" + self.requestline)
-        write_test("  Cmd: " + self.command)
-        write_test("  Path: " + self.path)
 
         contents = ""
         try:
@@ -44,21 +36,17 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 self.send_header('Content-Type', 'text/html')
             else:
                 end = self.path.split('?')[0]
-                write_test('Endpoint'+ end)
-                print("End =>", end)
                 if end == '/listSpecies':
                     contents = self.attend_info_species()
                     self.send_response(200)
                     self.send_header('Content-Type', 'text/html')
 
                 elif end == '/karyotype':
-                    write_test('Endpoint' + end)
                     contents = self.handle_info_assembly()
                     self.send_response(200)
                     self.send_header('Content-Type', 'text/html')
 
                 elif end == '/chromosomeLength':
-                    write_test('Endpoint' + end)
                     contents = self.handle_chromosome_length()
                     self.send_response(200)
                     self.send_header('Content-Type', 'text/html')
@@ -90,12 +78,10 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         request = SERVER + ENDPOINT[0]
         r = requests.get(request, headers=headers)
         print("Sending request:", request)
-        write_test("request: " + request)
         data = r.json()
-        write_test("Response: " + str(data))
 
         contents = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Species List</title></head>' \
-                   '<body style="background-color: white;"><h1>List of species</h1><ol>'
+                   '<body style="background-color: lime;"><h1>Species list:</h1><ol>'
 
         try:
             l = self.path.split('=')[1].split('&')[0]
@@ -125,27 +111,15 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
         request = SERVER + ENDPOINT[1] + "/" + specie
         print("Sending request:", request)
-
-        write_test('Request' + request)
-
         r = requests.get(request, headers = headers)
         data = r.json()
-        print("CONTENT: ")
-        print(data)
-
-        write_test('Parameters')
-        write_test('Specie:' + str(specie))
-        write_test('Response:' + str(data))
-
-        #http://rest.ensembl.org/info/assembly/homo_sapiens?
-
 
         contents = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Karyotype of ' + specie + '</title></head>' \
-                   '<body><h1>Karyotype of ' + specie + '</h1><ol>'
+                   '<body style="background-color: aquamarine;"><h1>Karyotype of ' + specie + '</h1><ol>'
 
         for index, elem in enumerate(data['karyotype']):
             contents += "<li>"
-            contents += elem #d['karyotype'][index]['common_name']
+            contents += elem
             contents += "</li>"
 
         contents += "</ol></body></html>"
@@ -155,33 +129,16 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def handle_chromosome_length(self):
 
-        #http://localhost:8000/chromosomeLength?specie=cat&chromo=11
-
         a = self.path.split('?')[1]
         b = a.split('&')
         specie = b[0].split('=')[1]
         chromo = b[1].split('=')[1]
-
         specie = specie.replace("+", "_")
-
-        print('specie = ', specie)
-        print('chromo =', chromo)
 
         request = SERVER + ENDPOINT[1] + "/" + specie
         print("Sending request:", request)
-
-        write_test('Request' + request)
-
         r = requests.get(request, headers=headers)
         data = r.json()
-        print("CONTENT: ")
-        print(data)
-
-        write_test('Parameters')
-        write_test('Specie:' + str(specie))
-        write_test('Chromo' + str(chromo))
-        write_test('Response' + str(data))
-
 
         lenchromo = ''
         for i in data['top_level_region']:
@@ -189,37 +146,28 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 if i['name'] == chromo:
                     lenchromo = i['length']
         if lenchromo == '':
-            contents = '<!DOCTYPE html><html lang="en" dir="ltr"><head>' \
-                       '<meta charset="UTF-8">' \
+            contents = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Error page</title></head>' \
+                        '<body style="background-color: red;"><h1>ERROR</h1>' \
+                        '<li> The name ' + chromo + ' is not valid or in the list.</li>' \
+                        '<li> Main page to try again: <li>' \
+                        '<a href = "http://localhost:8000">Link to the main page.</a></body></html>'
+            """'<!DOCTYPE html><html lang="en" dir="ltr"><head><meta charset="UTF-8">' \
                        '<title>ERROR</title>' \
                        '</head>' \
                        '<body style="background-color: red">' \
-                       '<h1>ERROR el nombre "' + chromo + '" no es válido.</h1>' \
-                        '<p>Here there are the websites available: </p>' \
-                        '<a href="/">[main server]</a></body></html>'
+                       '<h1>ERROR <h1>' \""""
+
         else:
-            contents = '<!DOCTYPE html> \
-                                            <html lang="en"> \
-                                            <head> \
-                                                <meta charset="UTF-8"> \
-                                                <title>LENGTH OF THE SELECTED CHROMOSOME</title> \
-                                            </head> \
-                                            <body style="background-color: lightblue;"> \
-                                            <body> \
-                                               The length of the chromosome is ' + str(lenchromo) + '. \
-                                            </body> \
-                                            </html>'
+            contents = '<!DOCTYPE html><html lang="en" dir="ltr"><head>'  \
+                       '<meta charset="UTF-8">' \
+                       '<title>Chromosome length</title>' \
+                       '<head>' \
+                        '<body style="background-color: yellow;">'\
+                       '<h1>Chromosome length</h1>'\
+                        '<body> The length of the chromosome is ' + str(lenchromo) + '.' \
+                        '</body></html>'
         return contents
 
-
-
-
-#test
-def write_test(cadena):
-    if TEST == True:
-        f = open('test_report.txt','a')
-        f.write(cadena + '\n')
-        f.close()
 
 Handler = TestHandler
 
